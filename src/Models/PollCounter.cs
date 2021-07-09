e24e992e13b0b1001784cddf2fbe904eab47cb39
RedStar <mutazzmutazz11@gmail.com>
2021-07-09 22:50:58 +0700

    added a new DbContext for poll counter number
    reference: https://github.com/redstarxx/DiscordBotOSIS/issues/7

---
+++ src/Models/PollCounter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace discordbot
+{
+    public class PollCounter
+    {
+        /// <summary>
+        /// The ID of the respective row. Do not touch.
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Event name to get or set.
+        /// </summary>
+        public int Counter { get; set; }
+    }
+}
---
+++ src/Models/PollCounterContext.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+#nullable disable
+
+namespace discordbot
+{
+    public partial class PollCounterContext : DbContext
+    {
+        /// <summary>
+        /// Provides a context to conduct database operations on the PollCounter table.
+        /// </summary>
+        public PollCounterContext()
+        {
+        }
+
+        public PollCounterContext(DbContextOptions<PollCounterContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<PollCounter> PollCounter { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                string connectionString = ClientUtilities.GetConnectionString();
+                optionsBuilder.UseNpgsql(connectionString);
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasPostgresExtension("adminpack")
+                .HasAnnotation("Relational:Collation", "English_United States.1252");
+
+            modelBuilder.Entity<PollCounter>(entity =>
+            {
+                entity.ToTable("pollcounter");
+
+                entity.Property(e => e.Id)
+                    .HasColumnName("id")
+                    .HasColumnType("int");
+
+                entity.Property(e => e.Counter)
+                    .HasColumnName("counter")
+                    .HasColumnType("int");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
