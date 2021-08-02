6874d75e3bab29c98db8c9798eee683ea13d7dfe
RedStar <mutazzmutazz11@gmail.com>
2021-08-02 17:27:42 +0700

    implementation of custom attributes
    RequireAccessRole, RequireAdminRole and RequireMainGuild substitutes the need for ClientUtilities' functions that achieves the exact thing

---
+++ src/Attributes/RequireAdminRole.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DSharpPlus;
+using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+
+namespace discordbot.Attributes
+{
+    [AttributeUsage(AttributeTargets.Method, AllowMultiple = false)]
+    public class RequireAdminRole : CheckBaseAttribute
+    {
+        public override Task<bool> ExecuteCheckAsync(CommandContext ctx, bool help)
+        {
+            bool hasServiceAdminRole = ctx.Member.Roles.Any(x => x.Name == "Service Administrator");
+
+            bool hasAdminRole = ctx.Member.Roles.Any(x => x.Name == "Administrator");
+
+            bool hasCoreCouncilRole = ctx.Member.Roles.Any(x => x.Name == "Inti OSIS");
+
+            bool permissionGranted = false;
+
+            if (!hasServiceAdminRole)
+            {
+                if (!hasAdminRole)
+                {
+                    if (!hasCoreCouncilRole)
+                    {
+                        permissionGranted = false;
+                    }
+
+                    else
+                    {
+                        permissionGranted = true;
+                    }
+                }
+
+                else
+                {
+                    permissionGranted = true;
+                }
+            }
+
+            else
+            {
+                permissionGranted = true;
+            }
+
+            return Task.FromResult(permissionGranted);
+        }
+    }
+}
---
+++ src/Attributes/RequireMainGuild.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DSharpPlus;
+using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+
+namespace discordbot.Attributes
+{
+    [AttributeUsage(AttributeTargets.Method, AllowMultiple = false)]
+    public class RequireMainGuild : CheckBaseAttribute
+    {
+        public override Task<bool> ExecuteCheckAsync(CommandContext ctx, bool help)
+        {
+            ulong mainGuildId = 814445508583358494;
+
+            return Task.FromResult(ctx.Guild.Id == mainGuildId);
+        }
+    }
+}
