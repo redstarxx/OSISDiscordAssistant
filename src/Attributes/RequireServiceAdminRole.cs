2fc52d4fc502bb024e7e13eaf3fccc8673c79598
RedStar <mutazzmutazz11@gmail.com>
2021-08-03 17:26:29 +0700

    RequireServiceAdminRole attribute

---
+++ src/Attributes/RequireServiceAdminRole.cs
@@ -0,0 +1,17 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+
+namespace discordbot.Attributes
+{
+    public class RequireServiceAdminRole : CheckBaseAttribute
+    {
+        public override Task<bool> ExecuteCheckAsync(CommandContext ctx, bool help)
+        {
+            bool isServiceAdmin = ctx.Member.Roles.Any(x => x.Name == "Service Administrator");
+
+            return Task.FromResult(isServiceAdmin);
+        }
+    }
+}
