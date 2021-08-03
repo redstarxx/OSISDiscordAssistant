5a71b7f1f4a1a363b2cb2f0e1e020c2995ce9443
RedStar <mutazzmutazz11@gmail.com>
2021-08-03 17:54:55 +0700

    RequireChannel attribute

---
+++ src/Attributes/RequireChannel.cs
@@ -0,0 +1,23 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+using DSharpPlus.Entities;
+
+namespace discordbot.Attributes
+{
+    public class RequireChannel : CheckBaseAttribute
+    {
+        public ulong channel { get; private set; }
+
+        public RequireChannel(ulong channelId)
+        {
+            channel = channelId;
+        }
+
+        public override Task<bool> ExecuteCheckAsync(CommandContext ctx, bool help)
+        {
+            return Task.FromResult(channel == ctx.Channel.Id);
+        }
+    }
+}
