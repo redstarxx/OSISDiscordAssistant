408a865323dbef9066dfee24ffdf80b0a264bd8c
RedStar <mutazzmutazz11@gmail.com>
2021-08-05 18:33:21 +0700

    added StringConstants.cs

---
+++ src/Constants/StringConstants.cs
@@ -0,0 +1,10 @@
+﻿namespace discordbot.Constants
+{
+    public static class StringConstants
+    {
+        /// <summary>
+        /// The output template DateTime format for Serilog.
+        /// </summary>
+        public const string LogDateTimeFormat = "[{Timestamp:dd-MM-yyyy HH:mm:ss} {Level:u3}] [{SourceContext}] {Message:lj}{NewLine}{Exception}";
+    }
+}
