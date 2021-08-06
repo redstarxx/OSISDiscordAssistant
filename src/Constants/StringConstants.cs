c940add083caeff18469d753467bfa7247e5f1b6
RedStar <mutazzmutazz11@gmail.com>
2021-08-06 08:38:57 +0700

    added const for MainGuildId

--- src/Constants/StringConstants.cs
+++ src/Constants/StringConstants.cs
@@ -6,5 +6,10 @@
         /// The output template DateTime format for Serilog.
         /// </summary>
         public const string LogDateTimeFormat = "[{Timestamp:dd-MM-yyyy HH:mm:ss} {Level:u3}] [{SourceContext}] {Message:lj}{NewLine}{Exception}";
+
+        /// <summary>
+        /// The guild ID of the main OSIS private server.
+        /// </summary>
+        public const ulong MainGuildId = 814445508583358494;
     }
 }
