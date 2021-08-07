2034a8c3baa24cee26812611fac7289da2a8fee5
RedStar <mutazzmutazz11@gmail.com>
2021-08-07 17:18:55 +0700

    included timezone into LogDateTimeFormat

--- src/Constants/StringConstants.cs
+++ src/Constants/StringConstants.cs
@@ -5,7 +5,7 @@
         /// <summary>
         /// The output template DateTime format for Serilog.
         /// </summary>
-        public const string LogDateTimeFormat = "[{Timestamp:dd-MM-yyyy HH:mm:ss} {Level:u3}] [{SourceContext}] {Message:lj}{NewLine}{Exception}";
+        public const string LogDateTimeFormat = "[{Timestamp:dd-MM-yyyy HH:mm:ss zzz} {Level:u3}] [{SourceContext}] {Message:lj}{NewLine}{Exception}";
 
         /// <summary>
         /// The guild ID of the main OSIS private server.
