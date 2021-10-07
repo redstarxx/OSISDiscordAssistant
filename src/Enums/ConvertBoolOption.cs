13b8f316b10fc17c8abab4d10e161a6dd2abe82b
RedStar <mutazzmutazz11@gmail.com>
2021-10-07 20:53:20 +0700

    add enums for ConvertBoolValue

---
+++ src/Enums/ConvertBoolOption.cs
@@ -0,0 +1,23 @@
+﻿namespace OSISDiscordAssistant.Enums
+{
+    /// <summary>
+    /// Specifies what to convert the bool value to.
+    /// </summary>
+    public enum ConvertBoolOption
+    {
+        /// <summary>
+        /// Converts the bool value to 'Yes' if true or 'No' if false.
+        /// </summary>
+        YesOrNo,
+
+        /// <summary>
+        /// Converts the bool value to 'Expired' if true or 'Active' if false.
+        /// </summary>
+        ActiveOrDone,
+
+        /// <summary>
+        /// Converts the bool value to 'Stored' if true or 'Not stored' if false.
+        /// </summary>
+        StoredOrNotStored
+    }
+}
