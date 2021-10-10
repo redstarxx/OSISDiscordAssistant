1873bdfd6aa8637bfaf3e71590513a096b186f12
RedStar <mutazzmutazz11@gmail.com>
2021-10-10 22:31:30 +0700

    moved Discord ID variables to SharedData

--- src/Constants/StringConstants.cs
+++ src/Constants/StringConstants.cs
@@ -6,25 +6,5 @@
         /// The output template DateTime format for Serilog.
         /// </summary>
         public const string LogDateTimeFormat = "[{Timestamp:dd-MM-yyyy HH:mm:ss zzz} {Level:u3}] [{EventId}] {Message:lj}{NewLine}{Exception}";
-
-        /// <summary>
-        /// The guild ID of the main OSIS private server.
-        /// </summary>
-        public const ulong MainGuildId = 814445508583358494;
-
-        /// <summary>
-        /// The channel ID of the events reminder messages channel.
-        /// </summary>
-        public const ulong EventChannel = 857589614558314575;
-
-        /// <summary>
-        /// The channel ID of the proposal submissions reminder messages channel.
-        /// </summary>
-        public const ulong ProposalChannel = 857589664269729802;
-
-        /// <summary>
-        /// The channel ID of the PRTask and ERTask exception logs channel.
-        /// </summary>
-        public const ulong ErrorChannel = 832172186126123029;
     }
 }
