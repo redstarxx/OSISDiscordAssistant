b56516e4c4c1e1a2030ea05013ff1656859cf839
RedStar <mutazzmutazz11@gmail.com>
2021-10-10 22:30:22 +0700

    add json property for Discord IDs

--- src/ConfigJSON.cs
+++ src/ConfigJSON.cs
@@ -7,19 +7,43 @@ namespace OSISDiscordAssistant
         /// <summary>
         /// Gets the bot's token from the config file.
         /// </summary>
-        [JsonProperty("token")]
+        [JsonProperty("Token")]
         public string Token { get; private set; }
 
         /// <summary>
         /// Gets the commands prefix.
         /// </summary>
-        [JsonProperty("prefix")]
+        [JsonProperty("Prefix")]
         public string Prefix { get; private set; }
 
         /// <summary>
         /// Gets the PostgreSQL database connection string required for database operations.
         /// </summary>
-        [JsonProperty("connectionstring")]
+        [JsonProperty("DbConnectionString")]
         public string ConnectionString { get; private set; }
+
+        /// <summary>
+        /// The guild ID of the main OSIS private server.
+        /// </summary>
+        [JsonProperty("MainGuildId")]
+        public ulong? MainGuildId { get; private set; }
+
+        /// <summary>
+        /// The channel ID of the events reminder messages channel.
+        /// </summary>
+        [JsonProperty("EventChannelId")]
+        public ulong? EventChannelId { get; private set; }
+
+        /// <summary>
+        /// The channel ID of the proposal submissions reminder messages channel.
+        /// </summary>
+        [JsonProperty("ProposalChannelId")]
+        public ulong? ProposalChannelId { get; private set; }
+
+        /// <summary>
+        /// The channel ID of the PRTask and ERTask exception logs channel.
+        /// </summary>
+        [JsonProperty("ErrorChannelId")]
+        public ulong? ErrorChannelId { get; private set; }
     }
 }
