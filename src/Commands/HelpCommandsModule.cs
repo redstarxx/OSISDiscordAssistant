d31e97d8dd428e7e463e8f07d52e56a4ce9cc4e7
RedStar <mutazzmutazz11@gmail.com>
2021-10-11 14:56:26 +0700

    divided all commands into corresponding fields

--- src/Commands/HelpCommandsModule.cs
+++ src/Commands/HelpCommandsModule.cs
@@ -22,34 +22,44 @@ namespace OSISDiscordAssistant.Commands
                 }
             };
 
-            string description = $"{Formatter.Bold("!event")} - Commands to access the Events Manager.\n" +
-                $"{Formatter.Bold("!remind")} - Reminder feature to remind yourself, a specific member or division, or everyone.\n" +
-                $"{Formatter.Bold("!poll")} - Creates a poll with a set of specified emojis to choose.\n" +
+            string councilCommands = $"{Formatter.Bold("!event")} - Commands to access the Events Manager.\n" +
+                $"{Formatter.Bold("!remind")} - Reminder feature to remind yourself, a specific member or division, or everyone.\n";
+
+            string generalCommands = $"{Formatter.Bold("!poll")} - Creates a poll with a set of specified emojis to choose.\n" +
                 $"{Formatter.Bold("!myinfo")} - Displays the info that this bot has over you.\n" +
                 $"{Formatter.Bold("!avatar")} - Shows a Discord profile picture of your account or another user.\n" +
-                $"{Formatter.Bold("!slap")} - Slaps the tagged member or role.\n" +
+                $"{Formatter.Bold("!uptime")} - Displays how long has the bot been running.\n" +
+                $"{Formatter.Bold("!ping")} - Displays the WebSocket connection latency in milliseconds.\n" +
+                $"{Formatter.Bold("!about")} - Shows the bot's information.\n";
+
+            string funCommands = $"{Formatter.Bold("!slap")} - Slaps the tagged member or role.\n" +
                 $"{Formatter.Bold("!flip")} - Flips a coin.\n" +
                 $"{Formatter.Bold("!snipe")} - Snipes a deleted message.\n" +
                 $"{Formatter.Bold("!snipeedit")} - Snipes the original content of an edited message.\n" +
                 $"{Formatter.Bold("!tags")} - Shows created tags.\n" +
-                $"{Formatter.Bold("!tag")} - Creates, updates, deletes, or mention a specified tag.\n" +
-                $"{Formatter.Bold("!setname")} - Sets a new name for yourself or another user.\n" +
+                $"{Formatter.Bold("!tag")} - Creates, updates, deletes, or mention a specified tag.\n";
+
+            string verificationCommands = $"{Formatter.Bold("!requestverify")} - Request a verification as a new member to the core council (Inti OSIS) members.\n" +
+                $"{Formatter.Bold("!overify")} - Manually verifies a new member as a council member.\n";
+
+            string administrationCommands = $"{Formatter.Bold("!setname")} - Sets a new name for yourself or another user.\n" +
                 $"{Formatter.Bold("!kick")} - Kicks a member from this Discord server.\n" +
                 $"{Formatter.Bold("!ban")} - Bans a member from this Discord server.\n" +
                 $"{Formatter.Bold("!unban")} - Unbans a member from this Discord server.\n" +
                 $"{Formatter.Bold("!mute")} - Mutes a member.\n" +
                 $"{Formatter.Bold("!unmute")} - Unmutes a member.\n" +
                 $"{Formatter.Bold("!prune")} - Prunes a channel from the specified message count to delete.\n" +
-                $"{Formatter.Bold("!announce")} - Sends an announcement message to a specified channel and role to mention.\n" +
-                $"{Formatter.Bold("!requestverify")} - Request a verification as a new member to the core council (Inti OSIS) members.\n" +
-                $"{Formatter.Bold("!overify")} - Manually verifies a new member as a council member.\n" +
-                $"{Formatter.Bold("!uptime")} - Displays how long has the bot been running.\n" +
-                $"{Formatter.Bold("!ping")} - Displays the WebSocket connection latency in milliseconds.\n" +
-                $"{Formatter.Bold("!eval")} - Evaluates a snippet of C# code.\n" +
-                $"{Formatter.Bold("!kill")} - Disconnects the bot from Discord's gateway.\n" +
-                $"{Formatter.Bold("!about")} - Shows the bot's information.\n";
+                $"{Formatter.Bold("!announce")} - Sends an announcement message to a specified channel and role to mention.\n";
 
-            embedBuilder.WithDescription(description);
+            string botAdminCommands = $"{Formatter.Bold("!eval")} - Evaluates a snippet of C# code.\n" +
+                $"{Formatter.Bold("!kill")} - Disconnects the bot from Discord's gateway.\n";
+
+            embedBuilder.AddField("Student Council Commands", councilCommands, false);
+            embedBuilder.AddField("General Commands", generalCommands, false);
+            embedBuilder.AddField("Fun Commands", funCommands, false);
+            embedBuilder.AddField("Verification Commands", verificationCommands, false);
+            embedBuilder.AddField("Administration Commands", administrationCommands, false);
+            embedBuilder.AddField("Bot Administration Commands", botAdminCommands, false);
             embedBuilder.Color = DiscordColor.MidnightBlue;
 
             await ctx.Channel.SendMessageAsync(embed: embedBuilder).ConfigureAwait(false);
