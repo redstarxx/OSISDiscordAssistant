72834ca6fe72278d83cb2626419e347bcb678c8d
RedStar <mutazzmutazz11@gmail.com>
2021-10-27 20:40:57 +0700

    add maxTimeSpanValue

---
+++ src/Constants/Constant.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace OSISDiscordAssistant.Constants
+{
+    public static class Constant
+    {
+        /// <summary>
+        /// The output template DateTime format for Serilog.
+        /// </summary>
+        public const string LogDateTimeFormat = "[{Timestamp:dd-MM-yyyy HH:mm:ss zzz} {Level:u3}] [{EventId}] {Message:lj}{NewLine}{Exception}";
+
+        /// <summary>
+        /// Maximum value of a TimeSpan instance.
+        /// </summary>
+        public static readonly TimeSpan maxTimeSpanValue = TimeSpan.FromMilliseconds(int.MaxValue);
+    }
+}
