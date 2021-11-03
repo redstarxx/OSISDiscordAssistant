fbf60d43c6be031b8bcb90ad1b470a258435e05e
RedStar <mutazzmutazz11@gmail.com>
2021-11-03 14:08:50 +0700

    add VerificationContext

---
+++ src/Models/Verification.cs
@@ -0,0 +1,25 @@
+﻿namespace OSISDiscordAssistant.Models
+{
+    public class Verification
+    {
+        /// <summary>
+        /// The ID of the respective row. Do not touch.
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// The user ID of the user requesting verification.
+        /// </summary>
+        public ulong UserId { get; set; }
+
+        /// <summary>
+        /// The ID of the verification request embed.
+        /// </summary>
+        public ulong VerificationEmbedId { get; set; }
+
+        /// <summary>
+        /// The new nickname requested by the requesting user. Can be null.
+        /// </summary>
+        public string RequestedName { get; set; }
+    }
+}
---
+++ src/Models/VerificationContext.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using OSISDiscordAssistant.Utilities;
+
+namespace OSISDiscordAssistant.Models
+{
+    public partial class VerificationContext : DbContext
+    {
+        /// <summary>
+        /// Provides a context to conduct database operations on the Events table.
+        /// </summary>
+        public VerificationContext()
+        {
+        }
+
+        public VerificationContext(DbContextOptions<VerificationContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Verification> Verifications { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                string connectionString = ClientUtilities.GetConnectionString();
+
+                // Retry reconnecting to the database on failure.
+                optionsBuilder.UseNpgsql(connectionString, builder => 
+                {
+                    builder.EnableRetryOnFailure(5, TimeSpan.FromSeconds(30), null);
+                });
+
+                base.OnConfiguring(optionsBuilder);
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasPostgresExtension("adminpack")
+                .HasAnnotation("Relational:Collation", "English_United States.1252");
+
+            modelBuilder.Entity<Verification>(entity =>
+            {
+                entity.ToTable("verification");
+
+                entity.Property(e => e.Id)
+                    .HasColumnName("id")
+                    .HasColumnType("int");
+
+                entity.Property(e => e.UserId)
+                    .HasColumnType("bigint")
+                    .HasColumnName("user_id");
+
+                entity.Property(e => e.VerificationEmbedId)
+                    .HasColumnType("bigint")
+                    .HasColumnName("verification_embed_id");
+
+                entity.Property(e => e.RequestedName)
+                    .HasColumnType("string")
+                    .HasColumnName("requested_nickname");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
