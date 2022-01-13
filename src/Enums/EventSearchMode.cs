974a40896e9612f4c6b41d7b22fccd988006a5f1
RedStar <mutazzmutazz11@gmail.com>
2022-01-13 14:00:59 +0700

    add EventSearchMode enum

---
+++ src/Enums/EventSearchMode.cs
@@ -0,0 +1,18 @@
+﻿namespace OSISDiscordAssistant.Enums
+{
+    /// <summary>
+    /// Specifies the strategy used to search for the event data.
+    /// </summary>
+    public enum EventSearchMode
+    {
+        /// <summary>
+        /// Search for an event with the exact name.
+        /// </summary>
+        Exact,
+
+        /// <summary>
+        /// Search for an event with the closest matching name.
+        /// </summary>
+        ClosestMatching
+    }
+}
