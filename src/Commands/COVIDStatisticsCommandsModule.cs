e1f715e7fa6f1e8cfa83e116ebaf792fb5601344
RedStar <mutazzmutazz11@gmail.com>
2022-01-24 20:50:44 +0700

    unnecessary line

--- src/Commands/COVIDStatisticsCommandsModule.cs
+++ src/Commands/COVIDStatisticsCommandsModule.cs
@@ -7,7 +7,6 @@ using DSharpPlus.Entities;
 using KawalCoronaSharp;
 using KawalCoronaSharp.Enums;
 
-
 namespace OSISDiscordAssistant.Commands
 {
     class COVIDStatisticsCommandsModule : BaseCommandModule
