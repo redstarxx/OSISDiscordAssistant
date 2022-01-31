8f74a2793d8aaf58eefc6b86984744421f824f23
RedStar <mutazzmutazz11@gmail.com>
2022-01-31 14:40:05 +0700

    include SourceContext into logging format

--- src/Constants/Constant.cs
+++ src/Constants/Constant.cs
@@ -7,12 +7,12 @@ namespace OSISDiscordAssistant.Constants
         /// <summary>
         /// The Serilog template format for writing logs into the console window.
         /// </summary>
-        public const string LogConsoleFormat = "[{@t:dd-MM-yyyy HH:mm:ss zzz} {@l:u3}]{#if EventId is not null} [{EventId.Name}]{#end} {@m}\n{@x}";
+        public const string LogConsoleFormat = "[{@t:dd-MM-yyyy HH:mm:ss zzz} {@l:u3}]{#if EventId is not null} [{EventId.Name}]{#end}{#if SourceContext is not null} [{Substring(SourceContext, LastIndexOf(SourceContext, '.') + 1)}]{#end} {@m}\n{@x}";
 
         /// <summary>
         /// The Serilog template format for writing logs into a text file.
         /// </summary>
-        public const string LogFileFormat = "[{Timestamp:dd-MM-yyyy HH:mm:ss zzz} {Level:u3}] {Message:lj}{NewLine}{Exception}";
+        public const string LogFileFormat = "[{Timestamp:dd-MM-yyyy HH:mm:ss zzz} {Level:u3}] [{SourceContext}] {Message:lj}{NewLine}{Exception}";
 
         /// <summary>
         /// Maximum value of a TimeSpan instance.
